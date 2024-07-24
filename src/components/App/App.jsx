583bdaaff8b59d62f82d29dd53665cfc22011002
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-07-24 21:31:16 +0300

    added dependencies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,16 @@
-export default function App() {}
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
+
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/MoviesPage">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
